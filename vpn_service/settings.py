9c6be495d488c7f83ef4181326cb343586ed6edb
eduardhabryd <eduardhabryd@gmail.com>
2023-11-27 15:32:13 +0200

    add users to installed apps

--- vpn_service/settings.py
+++ vpn_service/settings.py
@@ -15,7 +15,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "vpn_app"
+    "vpn_app",
+    "users",
 ]
 
 MIDDLEWARE = [
