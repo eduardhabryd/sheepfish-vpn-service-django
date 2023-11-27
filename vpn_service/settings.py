836f69c57febd7dc5379bc4c90c1a74bae278ea5
eduardhabryd <eduardhabryd@gmail.com>
2023-11-27 20:53:33 +0200

    configure templates in settings.py

--- vpn_service/settings.py
+++ vpn_service/settings.py
@@ -34,7 +34,7 @@ ROOT_URLCONF = "vpn_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
