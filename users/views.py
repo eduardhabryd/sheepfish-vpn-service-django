d45394427b980e850874178ac5b511bc3248e199
eduardhabryd <eduardhabryd@gmail.com>
2023-11-27 20:50:36 +0200

    implement views for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            # return redirect('home')  # Change 'home' to your actual home page URL
+            return HttpResponse("You were registered.")
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            # return redirect('home')  # Change 'home' to your actual home page URL
+            return HttpResponse("You were logged in.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    # return redirect('home')  # Change 'home' to your actual home page URL
+    return HttpResponse("You were logged out.")
