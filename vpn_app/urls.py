a867824c9dce777da5ab1fc66991d44fb9189fba
eduardhabryd <eduardhabryd@gmail.com>
2023-11-28 17:57:06 +0200

    update urlpatterns

--- vpn_app/urls.py
+++ vpn_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from vpn_app.views import SiteListView
+from vpn_app.views import SiteListView, SiteCreateView, SiteUpdateView, SiteDeleteView, site_proxy
 
 urlpatterns = [
-    path('', SiteListView.as_view()),
+    path('', SiteListView.as_view(), name='index'),
+    path('add_site/', SiteCreateView.as_view(), name='add_site'),
+    path("update_site/<int:pk>/", SiteUpdateView.as_view(), name='update_site'),
+    path("delete_site/<int:pk>/", SiteDeleteView.as_view(), name='delete_site'),
+    path('<str:user_site_name>/<path:routes_on_original_site>/', site_proxy, name='site_proxy'),
 ]
+
+
+app_name = 'vpn_app'
