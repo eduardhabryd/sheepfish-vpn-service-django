be6b4cac263cdb720ae1cb0f84179043fe6e3084
eduardhabryd <eduardhabryd@gmail.com>
2023-11-28 17:57:42 +0200

    update  site create view, update view and delete view; implement site_proxy function

--- vpn_app/views.py
+++ vpn_app/views.py
@@ -1,15 +1,14 @@
+import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
 
 from vpn_app.models import Site
-from vpn_app.forms import SiteSearchForm
+from vpn_app.forms import SiteSearchForm, SiteCreateForm, SiteUpdateForm
 
-
-# def index(request):
-#     user = request.user
-#     return render(request, 'vpn_app/index.html', {'user': user})
 
 class SiteListView(generic.ListView):
     model = Site
@@ -31,7 +30,49 @@ class SiteListView(generic.ListView):
         return self.queryset
 
 
-# @login_required
-# def personal_cabinet(request):
-#     user = request.user
-#     return render(request, 'vpn_app/personal_cabinet.html', {'user': user})
+class SiteCreateView(generic.CreateView):
+    model = Site
+    template_name = 'vpn_app/add_site.html'
+    form_class = SiteCreateForm
+    success_url = reverse_lazy('vpn_app:index')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class SiteUpdateView(generic.UpdateView):
+    model = Site
+    template_name = 'vpn_app/add_site.html'
+    form_class = SiteUpdateForm
+    success_url = reverse_lazy('vpn_app:index')
+
+
+class SiteDeleteView(generic.DeleteView):
+    model = Site
+    template_name = 'vpn_app/delete_site.html'
+    success_url = reverse_lazy('vpn_app:index')
+
+
+@csrf_exempt  # Use this decorator to disable CSRF protection for this view, as it's an external proxy
+def site_proxy(request, user_site_name, routes_on_original_site):
+    try:
+        # Assuming you have a user-specific site with the given name
+        site = Site.objects.get(user=request.user, name=user_site_name)
+
+        # Construct the URL of the original site based on user input
+        original_url = f"{site.url}/{routes_on_original_site}"
+
+        # Fetch content from the original site
+        response = requests.get(original_url)
+
+        # Modify the content to replace external links with internal routing
+        modified_content = response.text.replace('href="',
+                                                 'href="/{}/{}/'.format(user_site_name, routes_on_original_site))
+
+        return HttpResponse(modified_content)
+
+    except Site.DoesNotExist:
+        return HttpResponse("Site not found", status=404)
+    except Exception as e:
+        return HttpResponse(f"Error: {str(e)}", status=500)
