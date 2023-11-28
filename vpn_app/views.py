1dc699e3d64a5ffd5f773fce923d78d267b4e44e
eduardhabryd <eduardhabryd@gmail.com>
2023-11-28 20:38:33 +0200

    update site_proxy function to properly replace inner urls

--- vpn_app/views.py
+++ vpn_app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
+from urllib.parse import urlparse, urlunparse, urljoin, quote
 
 from vpn_app.models import Site
 from vpn_app.forms import SiteSearchForm, SiteCreateForm, SiteUpdateForm
@@ -54,25 +55,36 @@ class SiteDeleteView(generic.DeleteView):
     success_url = reverse_lazy('vpn_app:index')
 
 
-@csrf_exempt  # Use this decorator to disable CSRF protection for this view, as it's an external proxy
-def site_proxy(request, user_site_name, routes_on_original_site):
+@csrf_exempt
+def site_proxy(request, user_site_name, routes_on_original_site=""):
     try:
-        # Assuming you have a user-specific site with the given name
+        print(f"Request: {user_site_name}====={routes_on_original_site}")
+
+        if "/" in user_site_name:
+            split_user_site_name = user_site_name.split("/", 1)
+            user_site_name = split_user_site_name[0]
+            routes_on_original_site = split_user_site_name[1] + "/" + routes_on_original_site
+
         site = Site.objects.get(user=request.user, name=user_site_name)
 
-        # Construct the URL of the original site based on user input
-        original_url = f"{site.url}/{routes_on_original_site}"
+        original_url = urljoin(site.url, routes_on_original_site)
+        print(f"Original URL: {original_url}")
 
-        # Fetch content from the original site
         response = requests.get(original_url)
 
-        # Modify the content to replace external links with internal routing
-        modified_content = response.text.replace('href="',
-                                                 'href="/{}/{}/'.format(user_site_name, routes_on_original_site))
+        modified_url = f"/vpn/{user_site_name.rstrip('/')}"
+
+        modified_content = response.text.replace('href="', f'href="{modified_url}')
 
         return HttpResponse(modified_content)
 
     except Site.DoesNotExist:
-        return HttpResponse("Site not found", status=404)
+        print(f"Site not found for user: {user_site_name}")
+        return HttpResponse(
+            f"Site not found\n"
+            f"{user_site_name}=={routes_on_original_site}",
+            status=404
+        )
     except Exception as e:
+        print(f"Error: {str(e)}")
         return HttpResponse(f"Error: {str(e)}", status=500)
