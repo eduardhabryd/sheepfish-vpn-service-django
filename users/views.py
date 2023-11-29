ded8f10ddbd88e2349762073aad61128fe629acf
eduardhabryd <eduardhabryd@gmail.com>
2023-11-29 17:22:45 +0200

    update resiter, login and logout views

--- users/views.py
+++ users/views.py
@@ -1,19 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
+from users.forms import CustomUserCreationForm
 from django.contrib.auth import login, logout
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            # return redirect('home')  # Change 'home' to your actual home page URL
-            return HttpResponse("You were registered.")
+            return redirect('vpn_app:index')  # Change 'home' to your actual home page URL
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
 
@@ -23,8 +23,7 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            # return redirect('home')  # Change 'home' to your actual home page URL
-            return HttpResponse("You were logged in.")
+            return redirect('vpn_app:index')  # Change 'home' to your actual home page URL
     else:
         form = AuthenticationForm()
     return render(request, 'registration/login.html', {'form': form})
@@ -32,5 +31,4 @@ def user_login(request):
 
 def user_logout(request):
     logout(request)
-    # return redirect('home')  # Change 'home' to your actual home page URL
-    return HttpResponse("You were logged out.")
+    return redirect('vpn_app:index')  # Change 'home' to your actual home page URL
