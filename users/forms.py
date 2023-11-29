c756c756c2d858a36f1c872ab294086d8a975f5b
eduardhabryd <eduardhabryd@gmail.com>
2023-11-29 21:20:08 +0200

    implement CustomUserUpdateForm

--- users/forms.py
+++ users/forms.py
@@ -7,3 +7,9 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("email", "password1", "password2")
+
+
+class CustomUserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("email", "first_name", "last_name")
